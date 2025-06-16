0da9dbe3c0a32860a40412c0810525941159a7c1
Sahar <sahartich@gmail.com>
2025-06-16 14:48:05 +0300

    added main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "google" {
+}
+# Create a Google Cloud Storage bucket
+resource "google_storage_bucket" "empty_bucket" {
+  name          = "my-unique-2342342r2rr2dfgdgds" # Replace with a globally unique bucket name
+  location      = "me-west1"         # Should match the region specified in the provider or a multi-region location
+  storage_class = "STANDARD"                # Options: STANDARD, NEARLINE, COLDLINE, ARCHIVE
+  }
